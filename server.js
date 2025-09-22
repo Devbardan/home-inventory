afb28aa88e549b3b24fca0ffed3e3f38210b3d42
Daniel Velasquez <67128253+Devbardan@users.noreply.github.com>
2025-09-22 19:14:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { Pool } = pkg;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("."));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
